bea701de1e1447b9306d60ac71c60565c3fde95e
Teo Chiappero <teochiapps@gmail.com>
2026-02-26 22:56:43 -0300

    asd backend

---
+++ backend/src/api/proyecto/controllers/proyecto.ts
@@ -0,0 +1,7 @@
+/**
+ * proyecto controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::proyecto.proyecto');
---
+++ backend/src/api/proyecto/routes/proyecto.ts
@@ -0,0 +1,7 @@
+/**
+ * proyecto router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::proyecto.proyecto');
