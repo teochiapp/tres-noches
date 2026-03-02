c325050fc1d4f846ac0378dee22ad3af3beec215
Teo Chiappero <teochiapps@gmail.com>
2026-03-02 00:31:53 -0300

    asd

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
           <RightText
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.2 }}
+            transition={{ duration: 0.8 }}
           >
             <SplitText
               text="EMPEZAMOS DOCUMENTANDO HISTORIAS, TERMINAMOS TRANSFORMANDO TERRITORIOS."
